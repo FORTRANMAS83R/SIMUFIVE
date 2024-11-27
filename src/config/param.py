0159eb554d69063f4cfd547eb105f082fc4e8ca0
Gargamel <mikael.franco@imt-atlantique.fr>
2024-11-27 21:51:53 +0100

    .

--- src/config/param.py
+++ src/config/param.py
@@ -1,18 +1,10 @@
-global NB_HEURES_HC
-global NB_HEURES_HP
 
-global SEMAINE_NB_HC_JOUR
-global SEMAINE_NB_HP_JOUR
+NB_HEURES_HC = 25
+NB_HEURES_HP = 44 
 
-global WEEKEND_NB_HC_JOUR
-global WEEKEND_NB_HP_JOUR
-
-
-NB_HEURES_HC = 31
-NB_HEURES_HP = 61 
-
-SEMAINE_NB_HC_JOUR = 7
-SEMAINE_NB_HP_JOUR = 7
+#Pour le bar 
+SEMAINE_NB_HC_JOUR = 5
+SEMAINE_NB_HP_JOUR = 4
 
 WEEKEND_NB_HC_JOUR = 0
 WEEKEND_NB_HP_JOUR = 12 #Prendre en compte le fait qu'on ferme plus tôt le dimanche
